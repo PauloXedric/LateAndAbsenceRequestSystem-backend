2e114142d0239e575027de644c83bcd0f37e7a3e
PauloXedric <lozano.pauloxedric@gmail.com>
2025-06-07 00:10:45 +0800

    trying branches

--- Controller/RequestController.cs
+++ Controller/RequestController.cs
@@ -34,7 +34,7 @@ namespace DLARS.Controller
 
             if (result <= 0)
             {
-                return BadRequest("Error occured in adding a request");
+                return BadRequest("practicing branches");
             }
 
             return Ok(result);
