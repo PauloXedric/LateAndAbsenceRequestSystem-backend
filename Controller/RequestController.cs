0e4e0ecdd3ce76579cc110b9dfcc3b77a115de83
Paulo Xedric Lozano <lozano.pauloxedric@gmail.com>
2025-06-07 00:02:27 +0800

    Update RequestController.cs

--- Controller/RequestController.cs
+++ Controller/RequestController.cs
@@ -34,7 +34,7 @@ namespace DLARS.Controller
 
             if (result <= 0)
             {
-                return BadRequest("");
+                return BadRequest("Error occured in adding a request");
             }
 
             return Ok(result);
