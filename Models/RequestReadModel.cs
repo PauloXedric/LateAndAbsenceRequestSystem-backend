0565a38c4650fda3f7486df3f88f8b766a75033e
PauloXedric <lozano.pauloxedric@gmail.com>
2025-06-19 18:55:12 +0800

    Read Images

--- Models/RequestReadModel.cs
+++ Models/RequestReadModel.cs
@@ -4,31 +4,17 @@ namespace DLARS.Models
 {
     public class RequestReadModel
     {
-        [Required]
         public int RequestId { get; set; }
-
-        [Required]
         public string StudentNumber { get; set; } = string.Empty;
-
-        [Required]
         public string StudentName { get; set; } = string.Empty;
-
-        [Required]
         public string CourseYearSection { get; set; } = string.Empty;
-
-        [Required]
         public string Teacher { get; set; } = string.Empty;
-
-        [Required]
         public string SubjectCode { get; set; } = string.Empty;
-
-        [Required]
         public DateTime DateOfAbsence { get; set; } 
-
-        [Required]
         public DateTime DateOfAttendance { get; set; }
-
-        [Required]
         public string Reason { get; set; } = string.Empty;
+        public string ProofImage { get; set; } = string.Empty;
+        public string ParentValidImage { get; set; } = string.Empty;
+        public string? MedicalCertificate { get; set; }
     }
 }
